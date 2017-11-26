d8f148f64ce3c25a5d9df91cbb6da6c1fa6b116d
SeoHoYoung <ghdud2419@naver.com>
2017-11-27 02:26:10 +0900

    Add Chapter3 Finish

---
+++ Chapter3/Q8/Q3/Pratice1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+int getargs(char *cmd, char **argv);
+int main(){
+	char buf[256];
+	char *argv[50];
+	int narg;
+	int i;
+	pid_t pid;
+	
+	while (1){
+		printf("shell> ");
+		gets(buf);
+		if(strcmp(buf,"exit") == 0){
+			exit(1);
+		}
+		for(i = 0; i <strlen(buf);i++){
+			if(buf[i] == '&'){
+				buf[i]='\0';
+				break;
+			}
+		}
+		narg = getargs(buf, argv);
+		pid = fork();
+		
+		if (pid == 0){
+			execvp(argv[0], argv);
+			if(kill(pid, argv)){ printf("KIll\n");return 0;}
+		}
+		else if (pid > 0) wait((int *) 0);
+		else perror("fork failed");
+
+		
+	}
+	return 0;
+}
+
+int getargs(char *cmd, char **argv) {
+	int narg = 0;
+	while (*cmd) {
+		if (*cmd == ' ' || *cmd == '\t') *cmd++ = '\0';
+		else{ 
+			argv[narg++] = cmd++;
+			while (*cmd != '\0' && *cmd != ' ' && *cmd != '\t')
+				cmd++;
+		}
+	}
+	argv[narg] = NULL;
+
+	return narg;
+}
+
